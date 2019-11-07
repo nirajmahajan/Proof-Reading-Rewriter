7bc80e40cf963a2e2077eb3d39af352e91ff24c1
rishiA <rishiapril2000@gmail.com>
2019-11-07 11:26:58 +0530

    article checker for a and an

--- Rishi/article_checker.py
+++ Rishi/article_checker.py
@@ -1,10 +1,28 @@
 import nltk
+from nltk.tokenize.treebank import TreebankWordDetokenizer
 
 def art_check(s):
 	l = nltk.word_tokenize(s)
 	pos = nltk.pos_tag(l)
+	index = 0
+	vowels = ['a','e','i','o','u']
 	for i in pos:
+		index = index+1
 		if i[1]=='DT':
-			print("Yes")
+			try:
+				if pos[index][0][0] in vowels:
+					if(i[0]=='a'):
+						l[index-1] = 'an'
+					elif(i[0]=='A'):
+						l[index-1] = 'An'
+				else:
+					if(i[0]=='an'):
+						l[index-1] = 'a'
+					elif(i[0]=='An'):
+						l[index-1] = 'A'
+			except:
+				continue
 	print(pos)
+	rs = TreebankWordDetokenizer().detokenize(l)
+	return (rs)
 	pass
