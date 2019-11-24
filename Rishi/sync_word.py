94a421a9350e132262cd14fbec4a968993116571
rishiA <rishiapril2000@gmail.com>
2019-11-24 20:27:06 +0530

    now handles words like don't

--- Rishi/sync_word.py
+++ Rishi/sync_word.py
@@ -1,11 +1,14 @@
 import nltk
 
 def sync_word(w1, w2):
-	l1 = nltk.word_tokenize(w1)
+	l1 = nltk.pos_tag(nltk.word_tokenize(w1))
 	w = l1.pop(0)
 	ans = w2
-	if w[0].isupper():
+	if w[0][0].isupper():
 		ans = w2.capitalize()
+	else:
+		ans = w2.lower()
 	for i in l1:
-		ans = ans + i
+		if i[1] != 'RB':
+			ans = ans + i[0]
 	return ans
