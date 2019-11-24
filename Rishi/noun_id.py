f292a89e7eaddbd1e84e63ca802139d4ac356b83
rishiA <rishiapril2000@gmail.com>
2019-11-24 19:51:42 +0530

    noun_identifier made clean

--- Rishi/noun_id.py
+++ Rishi/noun_id.py
@@ -1,9 +1,6 @@
 import nltk
-from nltk.corpus import stopwords
 
-sT = set(stopwords.words('english'))
 noun = ['NN', 'NNS', 'NNP', 'NNPS']
-verbs = ['VB', 'VBP', 'VBZ', 'VBG', 'VBD']
 def noun_identifier(s):
 	l = nltk.word_tokenize(s)
 	tag = nltk.pos_tag(l)
