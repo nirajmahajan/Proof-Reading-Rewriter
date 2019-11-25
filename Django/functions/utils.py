ed5b658c74ae6e689b124732c24e07367eca195b
Neel Aryan <neelaryan2@gmail.com>
2019-11-25 12:02:41 +0530

    django update

--- Django/functions/utils.py
+++ Django/functions/utils.py
@@ -10,51 +10,61 @@ sys.path.insert(0, '../../')
 from main import *
 
 # made the strings raw
-alphabets= r"([A-Za-z])"
+alphabets = r"([A-Za-z])"
 prefixes = r"(Mr|St|Mrs|Ms|Dr)[.]"
 suffixes = r"(Inc|Ltd|Jr|Sr|Co)"
 starters = r"(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
 acronyms = r"([A-Z][.][A-Z][.](?:[A-Z][.])?)"
 websites = r"[.](com|net|org|io|gov)"
 
+
 def split_into_sentences(text):
     text = " " + text + "  "
-    text = text.replace("\r","")
-    text = text.replace("\n"," ")
-    text = re.sub(prefixes,"\\1<prd>",text)
-    text = re.sub(websites,"<prd>\\1",text)
-    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
-    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
-    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
-    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
-    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
-    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
-    if "”" in text: text = text.replace(".”","”.")
-    if "\"" in text: text = text.replace(".\"","\".")
-    if "!" in text: text = text.replace("!\"","\"!")
-    if "?" in text: text = text.replace("?\"","\"?")
-    text = text.replace(".",".<stop>")
-    text = text.replace("?","?<stop>")
-    text = text.replace("!","!<stop>")
-    text = text.replace("<prd>",".")
+    text = text.replace("\r", "")
+    text = text.replace("\n", " ")
+    text = re.sub(prefixes, "\\1<prd>", text)
+    text = re.sub(websites, "<prd>\\1", text)
+    if "Ph.D" in text: text = text.replace("Ph.D.", "Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] ", " \\1<prd> ", text)
+    text = re.sub(acronyms + " " + starters, "\\1<stop> \\2", text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]",
+                  "\\1<prd>\\2<prd>\\3<prd>", text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]", "\\1<prd>\\2<prd>",
+                  text)
+    text = re.sub(" " + suffixes + "[.] " + starters, " \\1<stop> \\2", text)
+    text = re.sub(" " + suffixes + "[.]", " \\1<prd>", text)
+    text = re.sub(" " + alphabets + "[.]", " \\1<prd>", text)
+    if "”" in text: text = text.replace(".”", "”.")
+    if "\"" in text: text = text.replace(".\"", "\".")
+    if "!" in text: text = text.replace("!\"", "\"!")
+    if "?" in text: text = text.replace("?\"", "\"?")
+    text = text.replace(".", ".<stop>")
+    text = text.replace("?", "?<stop>")
+    text = text.replace("!", "!<stop>")
+    text = text.replace("<prd>", ".")
     sentences = text.split("<stop>")
     sentences = sentences[:-1]
     sentences = [s.strip() for s in sentences]
-    sentences = [TreebankWordDetokenizer().detokenize(nltk.word_tokenize(s)) for s in sentences]
+    sentences = [
+        TreebankWordDetokenizer().detokenize(nltk.word_tokenize(s))
+        for s in sentences
+    ]
     return sentences
+
 
 def form_words(sent_list):
     l = []
     for x in sent_list:
-        l += x.split();
+        l += x.split()
     return l
 
+
 def getSuggestions(sentence, mode):
-    (ans, comment) = processSentence(x, 'grammar')
+    tempans = [[[] for x in sentence.split()], 1]
+    # (ans, comment) = processSentence(x, 'grammar')
     #returns [a, b] where a are suggestions and b is the mode of sentence currently
-    return [ans, 1]
+    # return [ans, 1]
+    return tempans
 
 def sync_word(w1, w2):
     l1 = nltk.pos_tag(nltk.word_tokenize(w1))
@@ -69,6 +79,7 @@ def sync_word(w1, w2):
             ans = ans + i[0]
     return ans
 
+
 def syn_list(word):
     url = "https://api.datamuse.com/words?ml=" + word
     response = urllib.request.urlopen(url)
@@ -78,6 +89,13 @@ def syn_list(word):
     for x in json_data:
         word_list.append(x['word'])
     return word_list
+
+
+def getPassive(sent_list):
+    return join(sent_list)
+
+def join(li):
+    return " ".join(li)
 
 # def rewrite(sentence):
 #     rewrite_types = [u'NN', u'NNS', u'JJ', u'JJS']
--- Django/main/views.py
+++ Django/main/views.py
@@ -1,162 +1,185 @@
 from django.shortcuts import render
 from functions.utils import *
-from bisect import bisect_left  
+from bisect import bisect_left
 
 global_sent_list = []
 global_types = []
 global_words = []
 global_suggestions = []
 
+
 # Create your views here.
 def main_view(request, *args, **kwargs):
-	global global_sent_list
-	global global_words
-	global global_types
-	default = ["hello yes", "world", "how"]
-	my_context = {}
-	my_context["my_list"] = default
-	my_context["words"] = []
-	my_context["original"] = ""
-	my_context["curr_word"] = "-1"
-	text = ""
-	recompute = 1
-	curr_word = -1
-	changed_sent = -1
-	replaced = -1
-	types = []
-	if request.method == "POST":
-		text = request.POST.get("text", None)
-		recompute = int(request.POST.get("recompute", None))
-		my_context["curr_word"] = request.POST.get("curr_word", None)
-		replaced = int(request.POST.get("replaced", None))
-		try:
-			curr_word = int(my_context["curr_word"])			
-		except:
-			pass
-		changed_sent = int(request.POST.get("changed", None)) 
-	my_context["original"] = text
-	sent_list = split_into_sentences(text)	
-	
-	if(recompute==0):
-		chInd = getIndex(sent_list, curr_word)
-		my_context["words"] = context_words()
-		my_context["my_list"] = default
-		# my_context["my_list"] = global_suggestions[chInd[0]][chInd[1]]
-		# load suggestions here, nothing else
-		return render(request, "main.html", my_context)
+    global global_sent_list
+    global global_words
+    global global_types
+    global global_suggestions
+    default = ["hello yes", "world", "how"]
+    my_context = {}
+    my_context["my_list"] = default
+    my_context["words"] = []
+    my_context["original"] = ""
+    my_context["curr_word"] = "-1"
+    my_context["passive"] = 0
+    my_context["passiveForm"] = ""
+    text = ""
+    recompute = 1
+    curr_word = -1
+    changed_sent = -1
+    replaced = -1
+    types = []
+    if request.method == "POST":
+        text = request.POST.get("text", None)
+        recompute = int(request.POST.get("recompute", None))
+        my_context["curr_word"] = request.POST.get("curr_word", None)
+        replaced = int(request.POST.get("replaced", None))
+        try:
+            curr_word = int(my_context["curr_word"])
+        except:
+            pass
+        changed_sent = int(request.POST.get("changed", None))
+    my_context["original"] = text
+    sent_list = split_into_sentences(text)
 
-	if(replaced!=-1):
-		if(curr_word==-1):
-			my_context["my_list"] = default
-			my_context["words"] = context_words()
-			return render(request, "main.html", my_context)
-		new_word = sync_word(global_words[curr_word], my_context["my_list"][replaced])
-		chInd = replace_in_sent(sent_list, curr_word, new_word)
-		global_words = form_words(sent_list)
-		my_context["original"] = ""
-		for i in sent_list:
-			my_context["original"] += " " + i
-		my_context["original"] = my_context["original"][1:]
-		my_context["my_list"] = default
-		# process here words and sent_list
-		mode = updateTypes(chInd, new_word)
-		[global_suggestions[chInd[0]], mode] = getSuggestions(global_sent_list[chInd[0]], mode)
-		temp = []
-		for x in global_types[chInd[0]]:
-			temp += [[x[0], mode]]
-		global_types[chInd[0]] = temp
-		curr_word = -1
-		my_context["words"] = context_words()
-		return render(request, "main.html", my_context)
-	
-	update(sent_list)
-	my_context["words"] = context_words()
-	return render(request, "main.html", my_context)
+    if (recompute == 0):
+        chInd = getIndex(sent_list, curr_word)
+        my_context["words"] = context_words()
+        my_context["my_list"] = default
+        # my_context["my_list"] = global_suggestions[chInd[0]][chInd[1]]
+        return render(request, "main.html", my_context)
+
+    elif (replaced != -1):
+        if (curr_word == -1):
+            my_context["my_list"] = default
+        else:
+            new_word = sync_word(global_words[curr_word],
+                                 my_context["my_list"][replaced])
+            chInd = replace_in_sent(sent_list, curr_word, new_word)
+            global_words = form_words(sent_list)
+            my_context["original"] = ""
+            for i in sent_list:
+                my_context["original"] += " " + i
+            my_context["original"] = my_context["original"][1:]
+            my_context["my_list"] = default
+            # process here words and sent_list
+            mode = updateTypes(chInd, new_word)
+            [global_suggestions[chInd[0]], mode] = getSuggestions(global_sent_list[chInd[0]], mode)
+            temp = []
+            for x in global_types[chInd[0]]:
+                temp += [[x[0], mode]]
+            global_types[chInd[0]] = temp
+            curr_word = -1
+    else:
+        update(sent_list)
+
+    my_context["passive"] = checkPassive()
+    if (my_context["passive"] == 1):
+        my_context["passiveForm"] = getPassive(sent_list)
+    else:
+        my_context["passiveForm"] = ""
+    # my_context["passive"] = 1
+    # my_context["passiveForm"] = getPassive(sent_list)
+    # print(my_context["passiveForm"])
+    my_context["words"] = context_words()
+    return render(request, "main.html", my_context)
 
 def context_words():
-	l=[]
-	types = [item[0] for sublist in global_types for item in sublist]
-	for x in range(0, len(types)):
-		l += [[global_words[x], types[x]]]
-	return l
+    l = []
+    types = [item[0] for sublist in global_types for item in sublist]
+    for x in range(0, len(types)):
+        l += [[global_words[x], types[x]]]
+    return l
 
 def update(sent_list):
-	global global_sent_list
-	global global_words
-	global global_suggestions
-	global global_types
-	sorted_sent = global_sent_list
-	sorted_sent.sort()
-	old = len(global_sent_list)
-	new = len(sent_list)
-	new_sugg = [[[] for x in y.split()] for y in sent_list]
-	new_type = [[[0,1] for x in y.split()] for y in sent_list]
-	for i in range(0, new):
-		ind = bisect_left(sorted_sent, sent_list[i])
-		if(ind==old):
-			# load new suggestions here
-			[new_sugg[i], mode] = getSuggestions(sent_list[i], 1)
-			new_type[i] = []
-			for x in new_sugg[i]:
-				if(x==[]):
-					new_type[i] += [[0, mode]]
-				else:
-					new_type += [mode, mode]
-		else:
-			new_sugg[i] = global_suggestions[ind]
-			new_type[i] = global_types[ind]
-	global_suggestions = new_sugg
-	global_sent_list = sent_list
-	global_words = form_words(global_sent_list)
-	global_types = new_type
-	
+    global global_sent_list
+    global global_words
+    global global_suggestions
+    global global_types
+    sorted_sent = global_sent_list
+    sorted_sent.sort()
+    old = len(global_sent_list)
+    new = len(sent_list)
+    new_sugg = [[[] for x in y.split()] for y in sent_list]
+    new_type = [[[0, 1] for x in y.split()] for y in sent_list]
+    for i in range(0, new):
+        ind = bisect_left(sorted_sent, sent_list[i])
+        if (ind == old):
+            # load new suggestions here
+            [new_sugg[i], mode] = getSuggestions(sent_list[i], 1)
+            new_type[i] = []
+            for x in new_sugg[i]:
+                if (x == []):
+                    new_type[i] += [[0, mode]]
+                else:
+                    new_type += [mode, mode]
+        else:
+            new_sugg[i] = global_suggestions[ind]
+            new_type[i] = global_types[ind]
+    global_suggestions = new_sugg
+    global_sent_list = sent_list
+    global_words = form_words(global_sent_list)
+    global_types = new_type
+
+
 def replace_in_sent(sent_list, curr_word, new_word):
-	global global_sent_list
-	ind = 0
-	ind2 = 0
-	y = []
-	for x in sent_list:
-		leng = len(x.split())
-		if(curr_word>=leng):
-			curr_word-=leng
-			ind+=leng
-			ind2+=1
-			continue
-		ind+=curr_word
-		y = x.split()
-		y[curr_word] = new_word
-		break
-	try:
-		sent_list[ind2]=""
-	except:
-		sent_list += [""]
-	for i in y:
-		sent_list[ind2] += " " + i
-	sent_list[ind2] = sent_list[ind2][1:]
-	global_sent_list[ind2] = sent_list[ind2]
-	return [ind2, curr_word]
+    global global_sent_list
+    ind = 0
+    ind2 = 0
+    y = []
+    for x in sent_list:
+        leng = len(x.split())
+        if (curr_word >= leng):
+            curr_word -= leng
+            ind += leng
+            ind2 += 1
+            continue
+        ind += curr_word
+        y = x.split()
+        y[curr_word] = new_word
+        break
+    try:
+        sent_list[ind2] = ""
+    except:
+        sent_list += [""]
+    for i in y:
+        sent_list[ind2] += " " + i
+    sent_list[ind2] = sent_list[ind2][1:]
+    global_sent_list[ind2] = sent_list[ind2]
+    return [ind2, curr_word]
+
 
 def getIndex(sent_list, curr_word):
-	ind2 = 0
-	for x in sent_list:
-		leng = len(x.split())
-		if(curr_word>=leng):
-			curr_word-=leng
-			ind2+=1
-			continue
-		break
-	return [ind2, curr_word]
+    ind2 = 0
+    for x in sent_list:
+        leng = len(x.split())
+        if (curr_word >= leng):
+            curr_word -= leng
+            ind2 += 1
+            continue
+        break
+    return [ind2, curr_word]
+
+
+def checkPassive():
+    try:
+    	m = min([min([x[1] for x in c]) for c in global_types])
+    except:
+    	return 0
+    if (m > 2):
+        return 1
+    else:
+        return 0
 
 def updateTypes(chInd, new_word):
-	global global_types
-	[i, j] = chInd
-	c = global_types[i][:j]
-	[a, b] = global_types[i][j]
-	for x in range(0, len(new_word.split())):
-		c += [[0, b+1]]
-	try:
-		c += global_types[i][j+1:]
-	except:
-		pass
-	global_types[i] = c
-	return min([x[1] for x in c])
+    global global_types
+    [i, j] = chInd
+    c = global_types[i][:j]
+    [a, b] = global_types[i][j]
+    for x in range(0, len(new_word.split())):
+        c += [[0, b + 1]]
+    try:
+        c += global_types[i][j + 1:]
+    except:
+        pass
+    global_types[i] = c
+    return min([x[1] for x in c])
